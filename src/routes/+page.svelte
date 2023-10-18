b63ce49c242979c75d2e03a19a198753448b2659
sixtendahlqvist <dahlqvistsixten93@gmail.com>
2023-10-18 09:32:29 +0200

    Klar med start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,30 @@
 
 <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
 
-<h3 >Förnamn Efternamn</h3>
+<h3 class = "name">Sixten Dahlqvist</h3>
 
+<p id="inspo">
+  "The only way to learn a new programming language is by writing
+  programs in it." - Dennis Ritchie, creator of the C programming language.
+  </p>
+  <p style="background-color:pink">
+  "Every great developer you know got there by solving problems they
+  were unqualified to solve until they actually did it." - Patrick McKenzie,
+  software engineer and entrepreneur.
+</p>
+
+<style>
+  h1{
+    color: grey
+  }
+  h2{
+    color:green
+  }
+  h3{
+    color:red
+  }
+  #inspo{
+  font-family: courier;
+  font-size: 160%;
+  }
+</style>
